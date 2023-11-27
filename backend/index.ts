3008eab83529a2e74f7544233beaa56c488b9072
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2023-11-27 17:08:32 +0900

    shopping app starting

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 const app = express();
+
 app.get("/", async (req, res) => {
     res.send("get starting.....")
-})
+});
+
 app.listen(5000, () => {
     console.log("server is running.....");
-})
+});
