8f4c84099c22ff6273cfced25cc402ae43e041ae
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2024-02-17 01:28:39 +0900

    upload photo cteate

--- frontend/src/components/addShirt.tsx
+++ frontend/src/components/addShirt.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Dialog, DialogContent, TextField } from "@mui/material";
 import { useState } from "react";
+import { config } from "../config/config";
+import FileUpload from "./FileUpload";
 
 interface Prop {
       open: boolean;
@@ -8,14 +10,28 @@ interface Prop {
 
 const AddShirt = ({ open, setOpen }: Prop) => {
       const [data, setData] = useState({ name: "", price: "" });
-      const [photo, setPhoto] = useState()
+      const [photo, setPhoto] = useState();
+      const [fileOpen, setFileOpen] = useState(false);
+      const addFunction = async () => {
+            if (!data.name) {
+                  return alert("Enter name");
+            } else if (!data.price) {
+                  return alert("Enter price");
+            };
+            const response = await fetch(`${config.apiBaseUrl}/addShirt`, {
+                  method: "POST",
+                  headers: { "content-type": "application/json" },
+                  body: JSON.stringify({ data }),
+            })
+      }
       return (
             <Dialog open={open} onClose={setOpen}>
                   <DialogContent sx={{ display: "flex", flexDirection: "column", p: 2, minWidth: 400, justifyContent: "center", alignItems: "center" }}>
-                        <TextField onChange={(evt) => { }} sx={{ width: 300, mt: 3 }} type="file" />
+                        <Button sx={{}} variant="contained" color="success" onClick={() => setFileOpen(true)}>&#65291; photo</Button>
+                        <FileUpload open={fileOpen} setOpen={() => setFileOpen(false)} />
                         <TextField sx={{ marginY: 2, minWidth: 300 }} onChange={(evt) => setData({ ...data, name: evt.target.value })} placeholder="name" />
                         <TextField sx={{ mb: 2, minWidth: 300 }} onChange={(evt) => setData({ ...data, price: evt.target.value })} placeholder="price" />
-                        <Button variant="contained">add</Button>
+                        <Button onClick={addFunction} variant="contained">add</Button>
                   </DialogContent>
             </Dialog>
       )
